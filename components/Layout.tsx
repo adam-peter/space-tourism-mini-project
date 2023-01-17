57fd3811848360c2ab1513052ea4feecdcce947f
badmood111 <petrzela.adam@seznam.cz>
2023-01-17 19:50:44 +0100

    Fix title

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import Header from './Header'
 
 type Props = {
-  children: JSX.Element,
+  children: JSX.Element
 }
 
 const Layout = ({children}: Props) => {
   return (
     <div>
-      <Header />
+      <Header/>
       {children}
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "@/styles/Home.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      
-      <main className="">
-        Hello!
-      </main>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main className="">Hello!</main>
     </>
   );
 }
